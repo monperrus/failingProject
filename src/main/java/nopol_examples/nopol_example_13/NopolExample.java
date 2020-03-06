02fe3c6f3b726f79f22295651ddf4daa02f044c1
lucesape <noreply@github.com>
2020-03-06 13:23:57 +0100

    Proposal for a patch

--- src/main/java/nopol_examples/nopol_example_13/NopolExample.java
+++ src/main/java/nopol_examples/nopol_example_13/NopolExample.java
@@ -1,8 +1,13 @@
 package nopol_examples.nopol_example_13;
 public class NopolExample {
     public boolean isEmpty(java.util.List list) {
-        if (list.isEmpty())
+        if (list == null) {
+            if ( new ArrayList().isEmpty())
             return true;
+        } else {
+            if (list.isEmpty())
+            return true;
+        }
         
         return false;
     }
